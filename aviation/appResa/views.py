fd7061a322af37e44cb1d00d5459a73749612ac5
guillaumecoin <guillaume.coin@epsi.fr>
2023-02-22 12:03:12 +0100

    affichage du login form

--- aviation/appResa/views.py
+++ aviation/appResa/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .forms import ReservationForm
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 #Requete du formulaire
 
 def reservation(request):
@@ -12,7 +14,12 @@ def reservation(request):
             return render(request, 'reservation_confirm.html')
         else:
             form = ReservationForm()
-        return render(request, 'reservation.html', {'form': form})
+        return render(request, 'appResa/reservation.html', {'form': form})
 
 def index(request):
     return HttpResponse("Hello World !")
+
+
+def login(request):
+    form = AuthenticationForm()
+    return render(request, 'appResa/login.html', {'form': form})
