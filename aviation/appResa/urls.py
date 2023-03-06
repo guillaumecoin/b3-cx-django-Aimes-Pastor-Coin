ca7bf0d92051fcd2d7d55d9f2bfbfa2817d68fe5
guillaumecoin <guillaume.coin@epsi.fr>
2023-03-06 20:05:50 +0100

    formulaire de connexion ok

--- aviation/appResa/urls.py
+++ aviation/appResa/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('index/', views.index, name='index'),
+    path('', views.login_vue, name='login'),
     path('accueil/', views.accueil, name='accueil'),
 ]
--- aviation/appResa/views.py
+++ aviation/appResa/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 from .forms import ReservationForm
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from .models import Ecole
+from .forms import LoginForm
 #Requete du formulaire
 
 def reservation(request):
@@ -21,8 +23,20 @@ def index(request):
     return HttpResponse("Hello World !")
 
 
-def login(request):
-    form = AuthenticationForm()
+def login_vue(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('accueil')
+            else:
+                form.add_error(None, 'incorrect')
+    else:
+        form = LoginForm()
     return render(request, 'appResa/login.html', {'form': form})
 
 def accueil(request):
